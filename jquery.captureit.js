e1ca1626b26100339377c3c05440152bafddc987
cuongpm <harry88pham@gmail.com>
2015-04-28 22:05:37 +0700

    fix to capture exactly content add call ajax

--- jquery.captureit.js
+++ jquery.captureit.js
@@ -3,20 +3,54 @@
     var settings = $.extend({
       border: '5px solid green',
       background: 'none',
-      ignore: ['comment']
+      ignores: 'comment|sidebar|aside|footer/i',
+      url: '',
+      data: {},
+      header: {},
+      ajax_type: 'post'
     }, options);
-    var divs = $('p').parent().parent(),
-        size = divs[0].innerHTML.length,
-        main_div = divs[0];
+
+    // Get page content via paragraph
+    var divs = $('p').parent().parent();
+
+    if(typeof divs[0] == "undefined" || divs[0].tagName.toLowerCase() == 'body'){
+      divs = $('div')[0].getElementsByTagName('div');
+    }
+
+    var size = divs[0].innerHTML.length,
+        main_div = divs[0],
+        main_width_div = divs[0].offsetWidth;
+
     $.each(divs, function(i, div){
-      if(div.innerHTML.trim().length > size){
-        size = div.innerHTML.trim().length
-        main_div = div;
+      var tagname = div.tagName.toLowerCase(),
+          classname = div.className.toLowerCase(),
+          idname    = div.id,
+          str_disable   = classname+idname+tagname;
+
+      if(str_disable.search(settings.ignores) == -1){
+        if(div.innerText.trim().length > size && div.offsetWidth > 0){
+          size = div.innerText.trim().length;
+          main_div = div;
+          main_width_div = div.offsetWidth;
+        }
       }
     });
 
     main_div.style.border = settings.border;
     main_div.style.background = settings.background;
+
+    // Call ajax
+    if(settings.url != '' && settings.data.size != 0){
+      $.ajax({
+        url: settings.url,
+        type: settings.ajax_type,
+        data: settings.data,
+        headers: settings.headers,
+        success: function (data) {
+          return {data: data, main_div: main_div};
+        }
+      });
+    }
     return main_div;
   }
 }(jQuery));
